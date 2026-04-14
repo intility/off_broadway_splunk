20fd84c5515196d4611b3c98a9ac8fc5627dc476
Rolf Håvard Blindheim <rolf.havard.blindheim@intility.no>
2026-04-14 13:40:33 +0200

    chore: Include credo in test environment
    Add :test to credo's only list to enable linting during test runs

--- mix.exs
+++ mix.exs
@@ -52,7 +52,7 @@ defmodule OffBroadway.Splunk.MixProject do
   defp deps do
     [
       {:broadway, "~> 1.0"},
-      {:credo, "~> 1.7", only: :dev},
+      {:credo, "~> 1.7", only: [:dev, :test]},
       {:dialyxir, ">= 0.0.0", only: [:dev, :test], runtime: false},
       {:decimal, "~> 2.0"},
       {:excoveralls, "~> 0.18.0", only: :test},
